ee23a8ed346f954a293543ca2120712f04308f86
pedromi92 <pedromi92@gmail.com>
2017-01-22 14:21:04 +0100

    pedromi

--- app/src/main/java/com/example/alber/mensfervidavideogame/MenuPrincipal.java
+++ app/src/main/java/com/example/alber/mensfervidavideogame/MenuPrincipal.java
@@ -9,5 +9,6 @@ public class MenuPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+        //hola by pedromi
     }
 }
