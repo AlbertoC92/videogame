c41e20d54410ad535a00c2929676c0c767887bfd
AlbertoC92 <alberto.g.castro92@gmail.com>
2017-01-22 12:51:30 +0100

    Update MenuPrincipal.java

--- app/src/main/java/com/example/alber/mensfervidavideogame/MenuPrincipal.java
+++ app/src/main/java/com/example/alber/mensfervidavideogame/MenuPrincipal.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MenuPrincipal extends AppCompatActivity {
-
+//Comentario de prueba de modificacion de codigo 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
